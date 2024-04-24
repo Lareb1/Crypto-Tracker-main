9fd31b60539e9a8dab9f92a00c48b5ecb6b0822f
Lareb1 <larebarain9@gmail.com>
2024-04-24 18:45:59 +0500

    first

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { makeStyles } from "@material-ui/core";
 import Homepage from "./Pages/HomePage";
 import "./App.css";
 import { BrowserRouter, Route } from "react-router-dom";
-import CoinPage from "./Pages/CoinPage";
+import CoinPage from "./Pages/CoinPage";;
 import Header from "./components/Header";
 
 const useStyles = makeStyles(() => ({
